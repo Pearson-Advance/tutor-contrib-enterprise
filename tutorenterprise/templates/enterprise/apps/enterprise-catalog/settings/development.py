54165bcd38887751a9152aa8c25774ace66ef15c
Javier Torres <javier.torres@edunext.co>
2023-12-28 15:11:48 -0500

    Co-authored-by: Andrés Felipe Bermúdez Mendoza <andres.bermudez@edunext.co> feat: add enterprise-catalog-worker deployment. feat: enable async enterprise-catalog-worker. feat: add hpa to enterprise-catalog. refactor: address suggestions. Update tutorenterprise/templates/enterprise/apps/enterprise-catalog/settings/partials/common.py

--- tutorenterprise/templates/enterprise/apps/enterprise-catalog/settings/development.py
+++ tutorenterprise/templates/enterprise/apps/enterprise-catalog/settings/development.py
@@ -3,5 +3,6 @@ from ..devstack import *
 {% include "enterprise/apps/enterprise-catalog/settings/partials/common.py" %}
 
 DISCOVERY_CATALOG_QUERY_CACHE_TIMEOUT = 0
+CELERY_TASK_ALWAYS_EAGER = True
 
 {{ patch("enterprise-catalog-development-settings") }}
